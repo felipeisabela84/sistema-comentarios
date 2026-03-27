57f86f2f23b234ad299868cf87fc28e5a1580b7b
felipeisabela84 <20251210@itla.edu.do>
2026-03-27 16:42:22 +0000

    index.php completado

--- index.php
+++ index.php
@@ -11,4 +11,46 @@
 </head>
 <body class="bg-light">
 
-<nav clas
+<nav class ="navbar navarbar-expand-lg navbar-light bg-white shadow-sm">
+    <div class= "container">
+        <a class="navbar-brand text-danger fw-bold" href="#">Inicio</a>
+        <div>
+        <a class="nav-link d-inline" href="#">Tiendas</a>
+      <a class="nav-link d-inline" href="#">Categorías</a>
+      <a class="nav-link d-inline" href="#">Ofertas</a>
+      <a class="nav-link d-inline" href="#">Contactos</a>
+    </div>
+  </div>
+</nav>
+<div class="container mt-4">
+<div class="container mt-4">
+
+<h2 class="fw-bold">Su opinión es importante</h2>
+
+<form action="guardar.php" method="POST" class="mt-3">
+    <label>Su nombre</label>
+    <input type="text" name="nombre" class="form-control mb-3" placeholder="Su nombre">
+
+    <label>Comentario</label>
+    <textarea name="comentario" class="form-control mb-3" rows="4"></textarea>
+
+    <button class="btn btn-primary">Enviar</button>
+</form>
+
+<span class="badge bg-secondary mt-3">Comentarios</span>
+
+<h5 class="mt-3 fw-bold">Comentarios recibidos</h5>
+
+<?php
+if(file_exists("comentarios.txt")){
+    $lineas = file("comentarios.txt");
+    foreach($lineas as $linea){
+        echo "<div class='comentario-box'>$linea</div>";
+    }
+}
+?>
+
+</div>
+
+</body>
+</html>
